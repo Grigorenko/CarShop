fa3f4db7307fd465452c6d8d02d56a4e771b66ab
Vitalik <ingenichsk@mail.ru>
2016-09-24 14:43:43 +0300

    Business Logic Layer
    Add Car service

---
+++ CarShop.BusinessLogic/Services/Implementation/CarService.cs
@@ -0,0 +1,53 @@
+﻿using CarShop.BusinessLogic.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using CarShop.Models;
+using System.Linq.Expressions;
+using CarShop.DataAccess.UnitOfWork;
+
+namespace CarShop.BusinessLogic.Services.Implementation
+{
+    public class CarService : ICarService
+    {
+        private IUnitOfWork _unit;
+        public CarService(IUnitOfWork unit)
+        {
+            _unit = unit;
+        }
+
+        public void Add(Car car)
+        {
+            _unit.GetRepository<Car>().Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            _unit.GetRepository<Car>().Delete(car);
+        }
+
+        public void Delete(int id)
+        {
+            _unit.GetRepository<Car>().Delete(id);
+        }
+
+        public ICollection<Car> Get()
+        {
+            return _unit.GetRepository<Car>().Get();
+        }
+
+        public Car Get(int id)
+        {
+            return _unit.GetRepository<Car>().Get(id);
+        }
+
+        public ICollection<Car> GetByPredicate(Expression<Func<Car, bool>> predicate)
+        {
+            return _unit.GetRepository<Car>().GetByPredicate(predicate);
+        }
+
+        public void Update(Car car)
+        {
+            _unit.GetRepository<Car>().Update(car);
+        }
+    }
+}
---
+++ CarShop.BusinessLogic/Services/Interfaces/ICarService.cs
@@ -0,0 +1,18 @@
+﻿using CarShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace CarShop.BusinessLogic.Services.Interfaces
+{
+    public interface ICarService
+    {
+        void Add(Car car);
+        Car Get(int id);
+        ICollection<Car> Get();
+        ICollection<Car> GetByPredicate(Expression<Func<Car, bool>> predicate);
+        void Delete(int id);
+        void Delete(Car car);
+        void Update(Car car);
+    }
+}
